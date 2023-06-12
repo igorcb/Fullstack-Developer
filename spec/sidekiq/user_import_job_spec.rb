aaf512a40eaae9f3e54c1b000118a7bef54f1793
Igor C. Batista <igor.batista@gmail.com>
2023-06-12 11:45:44 -0300

    create test user import job

--- spec/sidekiq/user_import_job_spec.rb
+++ spec/sidekiq/user_import_job_spec.rb
@@ -1,4 +1,21 @@
 require "rails_helper"
+require "sidekiq/testing"
+
 RSpec.describe UserImportJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform" do
+    subject { described_class.new }
+
+    let(:file_path) { Rails.root.join("spec/fixtures/files/user_import.xlsx") }
+    let(:upload) { create(:upload) }
+
+    it "calls UserImportService" do
+      user_import_service = instance_double(UserImportService)
+      expect(UserImportService).to receive(:new).with(file_path, upload.id).and_return(user_import_service)
+      expect(user_import_service).to receive(:call)
+
+      Sidekiq::Testing.inline! do
+        subject.perform(file_path, upload.id)
+      end
+    end
+  end
 end
